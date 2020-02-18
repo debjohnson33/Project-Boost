59f8647068c2a7c7862ac966ddbcee5b6c073c0c
Deb Johnson <friendswithdrj@gmail.com>
2020-02-18 15:08:08 -0600

    Adds collision control/changes rocket controls/adds backdrop

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,6 +7,8 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioData;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float rcsThrottle = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +24,28 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK"); // todo remove;
+                break;
+            case "Fuel":
+                print("Fuel");// log "dead"
+                break;
+            default:
+                print("Dead");
+                // todo kill player
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * rcsThrottle);
             if (!audioData.isPlaying)
             {
                 audioData.Play();
@@ -42,13 +61,15 @@ public class Rocket : MonoBehaviour
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation

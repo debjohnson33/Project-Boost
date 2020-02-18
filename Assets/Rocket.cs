e2c031efe7946601d22981b5943a2286fff2eb74
Deb Johnson <friendswithdrj@gmail.com>
2020-02-17 19:35:21 -0600

    Adds control of audio with space bar

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioData;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,14 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioData.isPlaying)
+            {
+                audioData.Play();
+            } 
+        }
+        else
+        {
+            audioData.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
